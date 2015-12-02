92430359d40b4f19b0d0e80b872b5fcc5725172c
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-12-02 06:45:19 +0100

    ci: Scaffold ecukes features tests

---
+++ features/support/env.el
@@ -0,0 +1,32 @@
+(require 'f)
+
+(defvar epresent-support-path
+  (f-dirname load-file-name))
+
+(defvar epresent-features-path
+  (f-parent epresent-support-path))
+
+(defvar epresent-root-path
+  (f-parent epresent-features-path))
+
+(add-to-list 'load-path epresent-root-path)
+
+(require 'epresent)
+(require 'espuds)
+(require 'ert)
+
+(Setup
+ ;; Before anything has run
+ )
+
+(Before
+ ;; Before each scenario is run
+ )
+
+(After
+ ;; After each scenario is run
+ )
+
+(Teardown
+ ;; After when everything has been run
+ )
